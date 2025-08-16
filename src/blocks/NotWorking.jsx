aa478a0cbf4b642dc3bef0bfb1083124c5e569f6
Selbi <selbibegiyeva1@gmail.com>
2025-08-16 12:32:23 +0500

    not working block

--- src/blocks/NotWorking.jsx
+++ src/blocks/NotWorking.jsx
@@ -19,9 +19,31 @@ function NotWorking() {
             <center>
                 <p className="working-head working-two" id="work-grad" style={{ maxWidth: 565 }}>universal "secrets" advice from books other people's success stories</p>
             </center>
-            <div className="not-working-block">
-                <p>It’s not working</p>
-            </div>
+            <center>
+                <div className="not-working-block">
+                    <div className='not-work-head'>
+                        <p>It’s not working</p>
+                    </div>
+                    <div className="not-work-point point-one">
+                        <span>Wait for the perfect moment and a genius idea</span>
+                    </div>
+                    <div className="not-work-point point-two">
+                        <span>Skip the latte, save $5 a day, and you’ll be a millionaire </span>
+                    </div>
+                    <div className="not-work-point point-three">
+                        <span>Drop out of school and you’ll be the next billionaire</span>
+                    </div>
+                    <div className="not-work-point point-four">
+                        <span>Just work hard and the money will come</span>
+                    </div>
+                    <div className="not-work-point point-five">
+                        <span>Rack up as many degrees as possible and a high salary is guaranteed</span>
+                    </div>
+                    <div className="not-work-point point-six">
+                        <span>Focus on your dream, and the Universe will attune itself to your success</span>
+                    </div>
+                </div>
+            </center>
             <center>
                 <div className="grad-line"></div>
             </center>
